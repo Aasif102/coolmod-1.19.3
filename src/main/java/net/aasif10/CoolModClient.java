c1b8af80fd766151c75dcf34c88f2dcbbf771fc2
unknown <AHMED@DESKTOP-291.291>
2024-04-04 18:37:26 -0400

    Initial commit

---
+++ src/main/java/net/aasif10/CoolModClient.java
@@ -0,0 +1,10 @@
+package net.aasif10;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class CoolModClient implements ClientModInitializer{
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
